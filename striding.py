52e46b811736ec47da9025411013b9ee1d442257
Kancherla Dhishitha <2420030314@klh.edu.in>
2026-06-30 20:01:19 +0530

    Add files via upload

---
+++ striding.py
@@ -0,0 +1,39 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #striding
+>>> a = 'date science'
+>>> a = 'data science'
+>>> a[::2]
+'dt cec'
+>>> a[::]
+'data science'
+>>> a = 'machine learning'
+>>> a[::5]
+'mnag'
+>>> a[::7]
+'m n'
+>>> a[::2]
+'mcielann'
+>>> a[3:11]
+'hine lea'
+>>> a[:8]
+'machine '
+>>> a[::4]
+'miln'
+>>> a[::10]
+'ma'
+>>> a[9:]
+'earning'
+>>> a = 'cloud computing'
+>>> a[1:9:2]
+'lu o'
+>>> a = cloud computing
+SyntaxError: invalid syntax
+>>> a[2:13:2]
+'odcmui'
+>>> a[6:14:4]
+'cu'
+>>> a[5:12:2]
+' opt'
+>>> a[3:9:5]
+'um'
